c50adcf4df8482534e3f06557e8224b7c705e3b5
lexxmg <lexx.mg@gmail.com>
2024-10-19 22:03:21 +0300

    paste to table

--- php/php-excel.php
+++ php/php-excel.php
@@ -15,6 +15,8 @@ $set = htmlspecialchars($_POST['set'] ?? '0');
 $path = $_SERVER['DOCUMENT_ROOT'] . '/doc-templates/template_act.xlsx';
 $saveName = $_SERVER['DOCUMENT_ROOT'] . "/storage/act/Акт № $contract АТС ТЕЛЕКОМПРОЕКТ $monthRod $yaer.xlsx";
 
+$tableJson = getStorage($jsonAct);
+
 // $reader = IOFactory::createReader('Xlsx');
 // $spreadsheet = $reader->load($path);
 $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($path);
@@ -30,27 +32,48 @@ $spreadsheet->getActiveSheet()->setCellValue('A7',
 );
 $spreadsheet->getActiveSheet()->setCellValue('A17',$summ);
 
+if ($tableJson) {
+	$tableJson[$set]['wasUsed'] = dateConvert($dateStart)['numDate'];
+
+	jsonToTable($set, $spreadsheet, $tableJson);
+}  
+
 $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
 $writer->setIncludeCharts(true);
 $writer->save($saveName);
 
 
-$tableJson = getStorage($jsonAct);
 
-if ($tableJson) {
-	$tableJson[$set]['wasUsed'] = dateConvert($dateStart)['numDate'];
-  $tableJson[$set]['data'][6] = [
-		'row' => 8,
-		'sort' => 3,
-		'cell' => [
-				'новая строка',
-				'шт.',
-				'17',
-				'3000',
-				'51'
-		]
-	];
-	setStorage($tableJson, $jsonAct);
-} else {
-	setStorage($table, $jsonAct);
+/**
+ * Замисывает в таблицу из JSON
+ * 
+ */
+function jsonToTable(int $set, object $spreadsheet, array $tableJson = []): bool
+{
+	$abc = array(
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+		'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+	);
+
+	$corStartLetter = $tableJson[$set]['firstCor'][0];
+	$corEndLetter = $tableJson[$set]['lastCor'][0];
+
+	$corStartNum = intval( substr($tableJson[$set]['firstCor'], 1) );
+	$corEndNum = intval( substr($tableJson[$set]['lastCor'], 1) );
+
+
+	for ($i=0; $i < ($corEndNum + 1) - $corStartNum; $i++) { 
+		foreach ($abc as $key => $letter) {
+			if ($letter >= $corStartLetter) {
+				$cellValue = $tableJson[$set]['data'][$i]['cell'][$key - 1];
+				$spreadsheet->getActiveSheet()->setCellValue($letter . $i + $corStartNum, $cellValue);
+			}
+
+			if ($letter === $corEndLetter) {
+				break;
+			}
+		}
+	}
+
+	return true;
 }
