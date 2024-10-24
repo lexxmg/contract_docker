e6065905f2b1abfdf79f9a537fbd73eb0bc814ef
work <lexx.mg@gmail.com>
2024-10-24 10:46:01 +0300

    first commit

--- templates/table.php
+++ templates/table.php
@@ -1,24 +1,27 @@
 <?php if ($arrayTable[$set]['edit']): ?>
   <form class="form-table-edit" action="" method="POST">
     <input type="hidden" name="set" value="<?=$set?>">
+
     <table class="form-create-table table-edit-js">
+      <thead class="form-create-table__thead">
+        <tr class="form-create-table__tr">
+          <?php foreach ($arrayTable[$set]['data'][0]['cell'] as $key => $cell): ?>
+            <th class="form-create-table__th <?php echo $key === 0 ? 'form-create-table__td--fix-size' : '' ?>"><?=$cell?></th>
+          <?php endforeach; ?>
+        </tr>
+      </thead>
+      
       <tbody class="form-create-table__tbody">
         <?php foreach ($arrayTable[$set]['data'] as $i => $value): ?>
-          <?php if ($value['row'] === 1): ?>
-            <thead class="form-create-table__thead">
-              <?php foreach ($value['cell'] as $key => $cell): ?>
-                <th class="form-create-table__th <?php echo $key === 0 ? 'form-create-table__td--fix-size' : '' ?>"><?= $cell ?></th>
-              <?php endforeach; ?>
-            </thead>
-          <?php else: ?>
-            <tr class="form-create-table__tr">
-              <?php foreach ($value['cell'] as $key => $cell): ?>
-                <td class="form-create-table__td <?php echo $key !== 0 ? 'form-create-table__td--text-centr' : '' ?>">
-                  <input class="form-table-edit__input" type="text" name="row_<?=$i?>[]" value="<?= $cell ?>" >
-                </td>
-              <?php endforeach; ?>
-            </tr>
-          <?php endif; ?>
+          <?php if ($value['row'] === 1) continue ?>
+          
+          <tr class="form-create-table__tr">
+            <?php foreach ($value['cell'] as $key => $cell): ?>
+              <td class="form-create-table__td <?php echo $key !== 0 ? 'form-create-table__td--text-centr' : '' ?>">
+                <input class="form-table-edit__input" type="text" name="row_<?=$i?>[]" value="<?=$cell?>" >
+              </td>
+            <?php endforeach; ?>
+          </tr>
         <?php endforeach; ?>
       </tbody>
     </table>
@@ -30,24 +33,24 @@
   </form>
 <?php else: ?>
   <table class="form-create-table">
+    <thead class="form-create-table__thead">
+      <tr class="form-create-table__tr">
+        <?php foreach ($arrayTable[$set]['data'][0]['cell'] as $key => $cell): ?>
+          <th class="form-create-table__th <?php echo $key === 0 ? 'form-create-table__td--fix-size' : '' ?>"><?=$cell?></th>
+        <?php endforeach; ?>
+      </tr>
+    </thead>
+
     <tbody class="form-create-table__tbody">
       <?php foreach ($arrayTable[$set]['data'] as $key => $value): ?>
-        <?php if ($value['row'] === 1): ?>
-          <thead class="form-create-table__thead">
-            <?php foreach ($value['cell'] as $key => $cell): ?>
-              <th class="form-create-table__th <?php echo $key === 0 ? 'form-create-table__td--fix-size' : '' ?>"><?= $cell ?></th>
-            <?php endforeach; ?>
-          </thead>
-        <?php else: ?>
-          <tr class="form-create-table__tr">
-            <?php foreach ($value['cell'] as $key => $cell): ?>
-              <td class="form-create-table__td <?php echo $key !== 0 ? 'form-create-table__td--text-centr' : '' ?>"><?= $cell ?></td>
-            <?php endforeach; ?>
-          </tr>
-        <?php endif; ?>
+        <?php if ($value['row'] === 1) continue ?>
+         
+        <tr class="form-create-table__tr">
+          <?php foreach ($value['cell'] as $key => $cell): ?>
+            <td class="form-create-table__td <?php echo $key !== 0 ? 'form-create-table__td--text-centr' : '' ?>"><?= $cell ?></td>
+          <?php endforeach; ?>
+        </tr>
       <?php endforeach; ?>
     </tbody>
   </table>
-<?php endif; ?>  
-
-<div class="create-table-js"></div>
+<?php endif; ?>
